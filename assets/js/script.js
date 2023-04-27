287b5e9af94b843bb0ee8b0fe1a523c1e87dbd76
ShyguyMatt <mjherford@gmail.com>
2023-04-27 09:16:55 -0400

    addedd js to display a question along with buttons for answers

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,21 +2,68 @@
 var goBtnEl = $('#go')
 var questionEl = $('#question')
 
-var questions = [question = {
-    question: "This is the question 1",
-    answer: "This is the answer",
-    false: "This is the wrong answer",
-    false2: "This is the wrong answer",
-    false3: "This is the wrong answer"
-}, question = {
-    question: "This is the question 2",
-    answer: "This is the answer",
-    false: "This is the wrong answer",
-    false2: "This is the wrong answer",
-    false3: "This is the wrong answer"
-}];
+var questions = [question = [
+    "This is the question 1",
+    "This is the answer",
+    "This is the wrong answer",
+    "This is the wrong answer",
+    "This is the wrong answer"
+], question = [
+    "This is the question 2",
+    "This is the answer",
+    "This is the wrong answer",
+    "This is the wrong answer",
+    "This is the wrong answer"
+], question = [
+    "This is the question 3",
+    "This is the answer",
+    "This is the wrong answer",
+    "This is the wrong answer",
+    "This is the wrong answer"
+], question = [
+    "This is the question 4",
+    "This is the answer",
+    "This is the wrong answer",
+    "This is the wrong answer",
+    "This is the wrong answer"
+], question = [
+    "This is the question 5",
+    "This is the answer",
+    "This is the wrong answer",
+    "This is the wrong answer",
+    "This is the wrong answer"
+]];
+
+function retrieveQuestion() {
+    r = getRandomInt(0, questions.length)
+    console.log(r)
+    questionEl.empty()
+    questionEl.append('<p>')
+    questionEl.children('p').text(questions[r][0])
+    for (var i = 1; i<questions[r].length; i++) {
+        questionEl.append("<div>");
+        questionEl.append("<button>");
+        questionEl.children('button').last().addClass('ansBtn');
+        questionEl.children('button').last().attr('text', questions[r][i]);
+        questionEl.children('button').last().text(questions[r][i]);
+        questionEl.children('div').last().addClass('break')
+    }
+}
+
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min) + min);
+  }
+
+questionEl.on('click', '.ansBtn', function (event) {
+    var text = event.target;
+    console.log($(event.target).attr('text'))
+    retrieveQuestion();
+})
 
 goBtnEl.on('click', function () {
     goBtnEl.css("display", "none")
+    retrieveQuestion();
     questionEl.css("display", "flex")
 })
