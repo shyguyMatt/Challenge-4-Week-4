2647993d5e9c0a575fd6b6aa3b9b5944a7618a1d
ShyguyMatt <mjherford@gmail.com>
2023-04-27 15:05:15 -0400

    timer implemented aswell as some logic for going to next question

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,38 +1,44 @@
 
 var goBtnEl = $('#go')
 var questionEl = $('#question')
+var headerEl = $('header')
+var timerEl = $('#timer')
+var fillEl = $('#fill')
+var correctAnswers = 0;
+var incorrectAnswers = 0;
+var time = 120 // in seconds
 
 // array of questions along with their answers
 var questions = [question = [
-    "This is the question 1",
-    "This is the answer",
-    "This is the wrong answer",
-    "This is the wrong answer",
-    "This is the wrong answer"
+    "What is JavaScript?",
+    "JavaScript is a scripting language used to make the website interactive",
+    "JavaScript is an assembly language used to make the website interactive",
+    "JavaScript is a compiled language used to make the website interactive",
+    "None of the mentioned"
 ], question = [
-    "This is the question 2",
-    "This is the answer",
-    "This is the wrong answer",
-    "This is the wrong answer",
-    "This is the wrong answer"
+    "Which of the following is correct about JavaScript?",
+    "JavaScript is an Object-Based language",
+    "JavaScript is Assembly-language",
+    "JavaScript is an Object-Oriented language",
+    "JavaScript is a High-level language"
 ], question = [
-    "This is the question 3",
-    "This is the answer",
-    "This is the wrong answer",
-    "This is the wrong answer",
-    "This is the wrong answer"
+    "Among the given statements, which statement defines closures in JavaScript?",
+    "JavaScript is a function that is enclosed with references to its lexical environment",
+    "JavaScript is a function that is enclosed with references to its inner function scope",
+    "JavaScript is a function that is enclosed with the object to its inner function scope",
+    "None of the mentioned"
 ], question = [
-    "This is the question 4",
-    "This is the answer",
-    "This is the wrong answer",
-    "This is the wrong answer",
-    "This is the wrong answer"
+    "Arrays in JavaScript are defined by which of the following statements?",
+    "It is an ordered list of values",
+    "It is an ordered list of objects",
+    "It is an ordered list of string",
+    "It is an ordered list of functions"
 ], question = [
-    "This is the question 5",
-    "This is the answer",
-    "This is the wrong answer",
-    "This is the wrong answer",
-    "This is the wrong answer"
+    "Which of the following is not javascript data types?",
+    "All of the mentioned",
+    "Number type",
+    "Undefined type",
+    "Null type"
 ]];
 
 // function retrieves a random questions and creates buttons
@@ -58,6 +64,7 @@ function retrieveQuestion() {
 
     // loops through the selected question and creates buttons with the
     // true and false options on them.
+    usedIndex = [];
     for (var i = 1; i<questions[r].length; i++) {
         //generates a new random array index if it has already been used
         do {
@@ -84,21 +91,58 @@ function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min) + min);
   }
 
-
 // listens to clicks on answer buttons, returns true if correct
 // otherwise false.
 questionEl.on('click', '.ansBtn', function (event) {
-    var text = event.target;
     console.log($(event.target).attr('text'))
-    if($(event.target).attr('ans') == true) console.log('correct')
-    else console.log('incorrect')
-    retrieveQuestion();
+    if($(event.target).attr('ans') == 'true') correct(event.target)
+    else incorrect(event.target);
+    //retrieveQuestion();
 })
-
 
 // listener for the start button. starts the application.
 goBtnEl.on('click', function () {
     goBtnEl.css("display", "none")
     retrieveQuestion();
     questionEl.css("display", "flex")
+    setInterval(timer, 1000);
+    headerEl.children().css('display', 'none')
+    timerEl.css('display', 'block')
+    var tempTimer = '00:00'
+    timerEl.text(tempTimer)   
 })
+
+function timer() {
+    var timer = ''
+    time--
+    var minutes = Math.floor(time/60)
+    var hours = Math.floor(minutes/60)
+
+    if (hours != 0) timer = timer + hours%60 + ":";
+    if (minutes != 0) {
+        if (minutes%60 < 10) timer = timer + "0" + minutes%60 + ":"
+        else timer = timer + minutes%60 + ":";
+    } else timer = timer + "0:";
+    if (time%60 < 10) timer = timer + "0" + time%60
+    else timer = timer + time%60
+    timerEl.text(timer)
+    if (time <= 0) endGame();
+}
+
+fillEl.on('click', function () {
+    fillEl.css('display', 'none')
+    retrieveQuestion();
+})
+
+function correct(clicked) {
+    $(clicked).css('background-color', 'green')
+    correctAnswers++
+    fillEl.css('display', 'block')
+}
+function incorrect(clicked) {
+    $("[ans=true]").css('background-color', 'green')
+    $(clicked).css('background-color', 'red')
+    incorrectAnswers++
+    time = time -5
+    fillEl.css('display', 'block')
+}
